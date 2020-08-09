11db89cf593eff888a0ef615780b5e83e76d3d08
banyue0618 <zsp_banyue@163.com>
2020-08-09 12:29:24 +0800

    git练习

---
+++ cloud-provider-payment8001/src/main/java/com/banyue/springcloud/PaymentApplication.java
@@ -0,0 +1,17 @@
+package com.banyue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author banyue
+ * date 2020-08-08
+ */
+@SpringBootApplication
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+}
